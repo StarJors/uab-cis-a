83162039d5d95201c110f7c4f6a6a4d51dd48fd0
StarJors <joseriverosuarez95@gmail.com>
2024-09-09 18:23:46 -0400

    cambios por si acaso

--- administrador/settings.py
+++ administrador/settings.py
@@ -141,14 +141,15 @@ USE_TZ = True
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
-
-
-STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True: 
 #    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
